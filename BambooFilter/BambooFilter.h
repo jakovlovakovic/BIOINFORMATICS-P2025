8dc80bb31c81e85649b2bbbc614a13a123e16252
Jakov Lovaković <jakov.lovakovic@fer.unizg.hr>
2025-06-01 17:36:08 +0200

    Changed visibility of some class members.

--- BambooFilter/BambooFilter.h
+++ BambooFilter/BambooFilter.h
@@ -9,17 +9,16 @@ struct Segment {
 	Segment(size_t num_of_buckets, size_t bucket_size);
 };
 
-// TODO: trebamo dodati i kriterij za koliko se insertiona radi ekspanzija
+// class for Bamboo Filter
 class BambooFilter {
-public: // TODO: vrati u private
+private:
 	size_t i, p, n0, lf, lb, ls0, Me, expansionTrigger; // runda ekspanzije, index sljedeceg za expand, pocetni broj segmenata, velicina fingertipa, velicina bucketa indexa, pocetna velicina segment indexa, varijable Me iz insert funkcije, expansion trigger
 	std::vector<Segment> segments; // segmenti
 	size_t getBitLength(uint32_t n); // helper funckija koja vraca bit length broja n
 	uint32_t reconstructHash(uint16_t f, uint32_t Is, uint16_t Ib); // helper funkcija koja rekonstruira hash
 
-//public:
+public:
 	BambooFilter();
-	//~BambooFilter();
 
 	uint16_t getFingerprint(std::string entry); // funkcija koja vraca fingertip za string entry
 	uint16_t getBucketIndex(std::string entry); // funkcija koja vraca bucket index za string entry
